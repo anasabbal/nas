d24d489477bf3c5f068bceff8c7385990092f487
anasabbal <anas.abbal66@gmail.com>
2021-10-19 15:56:58 +0100

    representing code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## NAS
 NAS Is a scripting language it executes directly from command line
+
+## Working with tree
--- src/main/java/com/universe/nas/Expr.java
+++ src/main/java/com/universe/nas/Expr.java
@@ -1,18 +1,75 @@
 package com.universe.nas;
 
+import java.util.List;
+
 /**
  * Expr is the base class that all expression inherit from
  */
 abstract class Expr {
-    static class Binary extends Expr{
-        final Expr left;
-        final Token operator;
-        final Expr right;
-
-        Binary(Expr left, Token operator, Expr right){
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+    //< expr-assign
+    //> expr-binary
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
             this.right = right;
         }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
     }
+    //> expr-grouping
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
+        final Expr expression;
+    }
+    //> expr-literal
+    static class Literal extends Expr {
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+
+        final Object value;
+    }
+    //> expr-unary
+    static class Unary extends Expr {
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+        final Token operator;
+        final Expr right;
+    }
+    //< expr-variable
+    abstract <R> R accept(Visitor<R> visitor);
 }
